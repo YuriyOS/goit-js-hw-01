e2d55089a0c2b4afcf6880f6f2c468a6f737cb5d
Юрий <urij@MacBook-Air-Urij.local>
2020-06-22 00:41:11 +0300

    #1_hw_v3

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,24 @@
 let credits = 23580;
+
 const pricePerDroid = 3000;
 
-let amount = prompt("Сколько нужно дроидов?");
+let totalPrice;
 
-let totalPrice = pricePerDroid * amount;
+let amount = prompt('Сколько нужно дроидов? ');
 
 if (amount === null) {
-  console.log("Отменено пользователем!");
-} else if (totalPrice > credits) {
-  console.log("Недостаточно средств на счету!");
+  console.log('Отменено пользователем');
 } else {
   amount = Number(amount);
   totalPrice = pricePerDroid * amount;
-  credits -= totalPrice;
-  console.log(
-    `Вы купили ${amount} дроидов, на счету осталось ${credits} кредитов`
-  );
+
+  if (totalPrice > credits ) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    credits -= totalPrice;
+    console.log(`Вы купили ${amount} дроидов, на счету осталось ${credits} кредитов.`)
+  }
 }
+
+
+
